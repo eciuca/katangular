8701ba3586664642ebc99279c3fe4c0903dcda47
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2023-11-22 00:50:00 +0200

    Add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavigationComponent} from '@kt/components/navigation/navigation.component';
 
 @Component({
   selector: 'kt-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
