474218afcde01c2795ad602600ac3acbad1d543a
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2023-11-23 12:25:22 +0200

    Some signals stuff

---
+++ src/app/components/playground/signals/signals.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'kt-signals',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.css'
+})
+export class SignalsComponent {
+
+
+  constructor() {
+  }
+}
--- src/app/components/students/student-details/student-details.component.ts
+++ src/app/components/students/student-details/student-details.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal, Signal, WritableSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentService } from '@kt/shared/student.service';
 import { Observable } from 'rxjs';
 import { Student } from '@kt/model/student';
 import { StudentComponent } from '@kt/components/students/student/student.component';
 import { MatCardModule } from '@angular/material/card';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'kt-student-details',
@@ -14,13 +15,15 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './student-details.component.css'
 })
 export class StudentDetailsComponent {
+  // studentIdValue: WritableSignal<number> = signal(-1);
+  student: WritableSignal<Student | undefined> = signal(undefined);
 
-  student$: Observable<Student> | undefined;
   constructor(private studentService: StudentService) {
   }
   @Input()
   set studentId(value: number) {
-    this.student$ = this.studentService.getStudent(value);
+    this.studentService.getStudent(value)
+      .subscribe((student) => this.student.set(student));
   }
 
 }
